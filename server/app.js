4ec4f2285824efc6695f77af4d2b10185edd8aef
Nazariy <nazar260804@gmail.com>
2023-01-19 21:51:22 +0200

    dependency and database setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express!')
-})
+const sequelize = require("./db");
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const start = async () => {
+  try { 
+ 
+    await sequelize.authenticate()
+    await sequelize.sync()
+ 
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start()
