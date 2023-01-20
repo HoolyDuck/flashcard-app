1e59eea129fb4dfbb163fa6d441e62624d7cbe07
Nazariy <nazar260804@gmail.com>
2023-01-20 22:20:34 +0200

    routes and some dances

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
-require('dotenv').config();
+require("dotenv").config()
 
 import { sequelize } from "./db";
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;  
+const cors = require("cors")
 const models = require("./models/models");
+import router  from "./routes/routes"
+
+app.use('/api', router)
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
   try {
