dac056bd1d52127bd3792409bf8b366e049f83c7
Nazariy <nazar260804@gmail.com>
2023-01-21 16:57:30 +0200

    created controllers and connected them to routes

--- server/src/routes/topicRouter.ts
+++ server/src/routes/topicRouter.ts
@@ -1,9 +1,13 @@
 import Router from "express"
 const router = Router()
+const topicController = require("../controllers/topicController")
 
-router.post('/')
-router.get('/')
-router.delete('/')
-router.put('/')
+
+router.get('/:id', topicController.get)
+router.get('/', topicController.getAll)
+router.post('/', topicController.create)
+router.put('/', topicController.update)
+router.delete('/', topicController.delete)
+
 
 module.exports = router
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,12 +1,10 @@
 import Router from "express"
 const router = Router()
+const userController = require("../controllers/userController")
 
-
-router.post('/register')
-router.post('/login')
-router.get('/auth', (req, res) => {
-    res.json({message: 'vac'})
-})
+router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.get('/auth', userController.auth)
 
 
 module.exports = router
