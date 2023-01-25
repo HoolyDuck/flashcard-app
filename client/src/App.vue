095ae10ec3309e953a5ed61975fb565b155bd338
Nazariy <nazar260804@gmail.com>
2023-01-26 01:12:12 +0200

    components created

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import Header from './components/Header.vue'
 
 </script>
 
 <template>
- 
+
+  <Header></Header>
+
+  <main>
+
+  </main>
+
 </template>
 
 <style scoped>
