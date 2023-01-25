fd3420aa1502a7f1e3229a0835c212d5f03c5f23
Nazariy <nazar260804@gmail.com>
2023-01-25 03:50:35 +0200

    added minimal user creation tool

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,18 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import { Request, Response } from "express-serve-static-core";
+
 class UserController {
 
-
     async register(req: Request, res: Response) {
-
+        // TODO: make it not ass and add validations
+        const {email, password} = req.body; 
+        //encrypt password !!!!!
+        await prisma.users.create({
+            data: { 
+                email: email,
+                password: password,
+            }
+        })
+        //add jwt 
+        res.send("Ok")
     }
 
     
     async login(req: Request, res: Response) {
-
+        // TODO: make it not ass and add validations
+        const {email, password} = req.body; 
+        const user = await prisma.users.findUnique({
+            where: {
+                email: email
+            }
+        })
+        //jwt and password validation
+        console.log(user)
+        res.send("Ok")
     }
 
     
     async auth(req: Request, res: Response) {
-
+        //check if user is authorized
     }
 }
 
