afde30e73821947da5d4d17a97c2c415b7ad1a53
Nazariy <nazar260804@gmail.com>
2023-01-25 20:04:20 +0200

    added middleware to check token

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,10 +1,12 @@
 import Router from "express"
 const router = Router()
 const userController = require("../controllers/userController")
+import { auth } from "../middleware/authMiddleware"
+
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
-router.get('/auth', userController.auth)
+router.get('/auth', auth , userController.auth)
 
 
 module.exports = router
