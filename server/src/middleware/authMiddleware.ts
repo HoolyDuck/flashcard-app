7d8f10cf8736f9e7e12e656fd3ba56ab9d213438
Nazariy <nazar260804@gmail.com>
2023-01-27 17:10:20 +0200

    made it so authMiddleware accepts auth header

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 
 export function auth(req: Request, res: Response, next: NextFunction) {
 
-    const token = req.header('x-auth-token');
+    const token = req.header('Authorization').split(' ')[1];
     if(!token) return res.status(401).send('Access denied. No token provided.');
     try {
         const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
