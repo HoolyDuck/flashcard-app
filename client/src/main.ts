39399aee85788e2e67464521f1b19c566696f3d1
Nazariy <nazar260804@gmail.com>
2023-01-29 14:56:18 +0200

    font awesome icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,21 +2,30 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/main.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 import routes from "./routes/routes";
 import { createRouter, createWebHistory } from "vue-router";
 import { createPinia } from "pinia";
 
-const router = createRouter({   
-    history: createWebHistory(),
-    routes,
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
 
-const pinia = createPinia()
+const pinia = createPinia();
 
+library.add(
+    faAt,
+    faLock
+    );
 
-const app = createApp(App); 
+const app = createApp(App);
 app.use(router);
 app.use(pinia);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
