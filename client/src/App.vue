268dae62313ca300165a38925be798816158a1a1
Nazariy <nazar260804@gmail.com>
2023-01-31 22:20:51 +0200

    import cleanup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
-import { useAuthStore } from "@/store/authStore";
-import { onBeforeMount } from 'vue'
-import { auth } from "./http/userApi"
-
-const authStore = useAuthStore();
 
 </script>
 
